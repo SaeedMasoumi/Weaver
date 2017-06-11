6b712ba89c0418c424a160fdf532e52330571453
Saeed Masoumi <s-masoumi@live.com>
2017-06-11 13:07:14 +0430

    Update javassist and android gradle

--- weaver-plugin/src/main/groovy/weaver/plugin/task/AndroidTransformerTask.groovy
+++ weaver-plugin/src/main/groovy/weaver/plugin/task/AndroidTransformerTask.groovy
@@ -24,205 +24,206 @@ import static weaver.plugin.util.UrlUtils.normalizeDirectoryForClassLoader
  */
 public class AndroidTransformerTask extends Transform {
 
-    Project project
-    Logger logger
+  Project project
+  Logger logger
 
-    AndroidTransformerTask(Project project) {
-        this.project = project
-        this.logger = project.logger
-    }
+  AndroidTransformerTask(Project project) {
+    this.project = project
+    this.logger = project.logger
+  }
 
-    @Override
-    String getName() {
-        return "weaver"
-    }
+  @Override
+  String getName() {
+    return "weaver"
+  }
 
-    @Override
-    Set<ContentType> getInputTypes() {
-        return ImmutableSet.<ContentType> of(DefaultContentType.CLASSES)
-    }
+  @Override
+  Set<ContentType> getInputTypes() {
+    return ImmutableSet.<ContentType> of(DefaultContentType.CLASSES)
+  }
 
-    @Override
-    Set<Scope> getScopes() {
-        return Sets.immutableEnumSet(Scope.PROJECT)
-    }
+  @Override
+  Set<Scope> getScopes() {
+    return Sets.immutableEnumSet(Scope.PROJECT)
+  }
 
-    @Override
-    Set<Scope> getReferencedScopes() {
-        return Sets.immutableEnumSet(Scope.EXTERNAL_LIBRARIES,
-                Scope.PROJECT_LOCAL_DEPS,
-                Scope.SUB_PROJECTS,
-                Scope.SUB_PROJECTS_LOCAL_DEPS,
-                Scope.TESTED_CODE)
-    }
+  @Override
+  Set<Scope> getReferencedScopes() {
+    return Sets.immutableEnumSet(Scope.EXTERNAL_LIBRARIES,
+        Scope.EXTERNAL_LIBRARIES,
+        Scope.SUB_PROJECTS,
+        Scope.TESTED_CODE)
+  }
 
-    @Override
-    boolean isIncremental() {
-        return false
-    }
+  @Override
+  boolean isIncremental() {
+    return false
+  }
 
-    @Override
-    void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
-        TransformBundle bundle = createTransformBundle(transformInvocation)
-        WeaverExec weaverExec = new WeaverExec(bundle)
-        try {
-            def doLast = {
-                Set<CtClass> allClasses ->
-                    def path = getOutputFile(transformInvocation.outputProvider).canonicalPath
-                    allClasses.each {
-                        it.writeFile(path)
-                    }
-                    log "All classes copied into $path"
-            }
-            weaverExec.execute(doLast)
-        } catch (all) {
-            log "an error occurred during transformation [ $all.message ] "
+  @Override
+  void transform(TransformInvocation transformInvocation)
+      throws TransformException, InterruptedException, IOException {
+    TransformBundle bundle = createTransformBundle(transformInvocation)
+    WeaverExec weaverExec = new WeaverExec(bundle)
+    try {
+      def doLast = { Set<CtClass> allClasses ->
+        def path = getOutputFile(transformInvocation.outputProvider).canonicalPath
+        allClasses.each {
+          it.writeFile(path)
         }
-        copyResourceFiles(transformInvocation.inputs, transformInvocation.outputProvider)
-        weaverExec.dispose()
-        bundle.dispose()
-        log "ClassPool and ClassLoaders are disposed successfully"
+        log "All classes copied into $path"
+      }
+      weaverExec.execute(doLast)
+    } catch (all) {
+      log "an error occurred during transformation [ $all.message ] "
     }
+    copyResourceFiles(transformInvocation.inputs, transformInvocation.outputProvider)
+    weaverExec.dispose()
+    bundle.dispose()
+    log "ClassPool and ClassLoaders are disposed successfully"
+  }
 
-    TransformBundle createTransformBundle(TransformInvocation transformInvocation) {
-        URLClassLoader rootClassLoader = createClassLoader(transformInvocation.referencedInputs)
-        WeaverClassPool pool = createClassPool(rootClassLoader, transformInvocation.inputs, transformInvocation.referencedInputs)
-        appendBootClassPath(pool)
-        TransformBundle bundle = TransformBundleImp.builder()
-                .project(project)
-                .configuration(project.configurations.getByName(WeaverPlugin.WEAVER_CONFIGURATION))
-                .rootClassLoader(rootClassLoader)
-                .classPool(pool)
-                .classFiles(getClassFiles(transformInvocation.inputs))
-                .outputDir(getSecondaryOutputFile())
-                .build()
-        return bundle
+  TransformBundle createTransformBundle(TransformInvocation transformInvocation) {
+    URLClassLoader rootClassLoader = createClassLoader(transformInvocation.referencedInputs)
+    WeaverClassPool pool = createClassPool(rootClassLoader, transformInvocation.inputs,
+        transformInvocation.referencedInputs)
+    appendBootClassPath(pool)
+    TransformBundle bundle = TransformBundleImp.builder()
+        .project(project)
+        .configuration(project.configurations.getByName(WeaverPlugin.WEAVER_CONFIGURATION))
+        .rootClassLoader(rootClassLoader)
+        .classPool(pool)
+        .classFiles(getClassFiles(transformInvocation.inputs))
+        .outputDir(getSecondaryOutputFile())
+        .build()
+    return bundle
+  }
+
+  URLClassLoader createClassLoader(Collection<TransformInput> referencedInputs) {
+    def urls = []
+    referencedInputs.each {
+      it.directoryInputs.each {
+        urls += normalizeDirectoryForClassLoader(it.file)
+      }
+      it.jarInputs.each {
+        urls += it.file.toURI().toURL()
+      }
     }
+    //TODO check whether it's needed to add classes directory or not
+    project.android.bootClasspath.each {
+      String path = it.absolutePath
+      urls += project.file(path).toURI().toURL()
+      log "Add android boot class [$path] to the weaver class loader."
+    }
+    return new URLClassLoader(urls as URL[], Thread.currentThread().contextClassLoader)
+  }
 
-    URLClassLoader createClassLoader(Collection<TransformInput> referencedInputs) {
-        def urls = []
-        referencedInputs.each {
-            it.directoryInputs.each {
-                urls += normalizeDirectoryForClassLoader(it.file)
-            }
-            it.jarInputs.each {
-                urls += it.file.toURI().toURL()
-            }
+  void appendBootClassPath(WeaverClassPool classPool) {
+    project.android.bootClasspath.each {
+      classPool.appendClassPath(it as File)
+      log "Add android boot class [$it] to the weaver class pool."
+    }
+  }
+
+  /**
+   * Creates a {@link javassist.ClassPool}. all class paths in source code,
+   * dependencies, android boot class path and system class path will be appended to  class pool.
+   *
+   * @param classLoader Class loader of class pool.
+   * @param inputs the source inputs provided by transform API.
+   * @param referencedInputs the dependencies inputs provided by transform API.
+   * @return returns a {@link WeaverClassPool}
+   */
+  private
+  static WeaverClassPool createClassPool(URLClassLoader classLoader,
+      Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs) {
+    WeaverClassPool pool = new WeaverClassPool(classLoader)
+    pool.childFirstLookup = true
+    pool.appendSystemPath()
+    inputs.each {
+      it.jarInputs.each {
+        pool.appendClassPath(it.file)
+      }
+      it.directoryInputs.each {
+        pool.appendClassPath(it.file)
+      }
+    }
+    referencedInputs.each {
+      it.jarInputs.each {
+        pool.appendClassPath(it.file)
+      }
+      it.directoryInputs.each {
+        pool.appendClassPath(it.file)
+      }
+    }
+    return pool
+  }
+
+  /**
+   * @param inputs the inputs provided by transform API.
+   * @return Returns all .class files from project source set.
+   */
+  private static Set<File> getClassFiles(Collection<TransformInput> inputs) {
+    Set<File> classFiles = new HashSet<File>()
+    inputs.each {
+      it.directoryInputs.each {
+        it.file.eachFileRecurse(FileType.FILES) {
+          if (it.absolutePath.endsWith(SdkConstants.DOT_CLASS)) {
+            classFiles.add(it)
+          }
         }
-        //TODO check whether it's needed to add classes directory or not
-        project.android.bootClasspath.each {
-            String path = it.absolutePath
-            urls += project.file(path).toURI().toURL()
-            log "Add android boot class [$path] to the weaver class loader."
+      }
+      it.jarInputs.each {
+        def jarFile = new JarFile(it.file)
+        jarFile.entries().findAll {
+          !it.directory && it.name.endsWith(SdkConstants.DOT_CLASS)
+        }.each {
+          classFiles.add(new File(it.name))
         }
-        return new URLClassLoader(urls as URL[], Thread.currentThread().contextClassLoader)
+      }
     }
+    return classFiles
+  }
 
+  /**
+   * @param outputProvider
+   * @return Returns a directory which will store transformed classes.
+   */
+  private File getOutputFile(TransformOutputProvider outputProvider) {
+    return outputProvider.getContentLocation('weaver', getInputTypes(), getScopes(),
+        Format.DIRECTORY)
+  }
 
-    void appendBootClassPath(WeaverClassPool classPool) {
-        project.android.bootClasspath.each {
-            classPool.appendClassPath(it as File)
-            log "Add android boot class [$it] to the weaver class pool."
+  private File getSecondaryOutputFile() {
+    return project.file("$project.buildDir/weaver/")
+  }
+
+  /**
+   * Copies all non-class files.
+   * @param inputs
+   * @param outputProvider
+   * @return
+   */
+  private copyResourceFiles(Collection<TransformInput> inputs,
+      TransformOutputProvider outputProvider) {
+    inputs.each {
+      it.directoryInputs.each {
+        def dirPath = it.file.absolutePath
+        it.file.eachFileRecurse(FileType.FILES) {
+          if (!it.absolutePath.endsWith(SdkConstants.DOT_CLASS)) {
+            log "Copying resource ${it}"
+            def dest = new File(getOutputFile(outputProvider),
+                it.absolutePath.substring(dirPath.length()))
+            dest.parentFile.mkdirs()
+            Files.copy(it, dest)
+          }
         }
+      }
+      // no need to implement the code for `it.jarInputs.each` since PROJECT SCOPE does not use jar input.
     }
+  }
 
-    /**
-     * Creates a {@link javassist.ClassPool}. all class paths in source code,
-     * dependencies, android boot class path and system class path will be appended to  class pool.
-     *
-     * @param classLoader Class loader of class pool.
-     * @param inputs the source inputs provided by transform API.
-     * @param referencedInputs the dependencies inputs provided by transform API.
-     * @return returns a {@link WeaverClassPool}
-     */
-    private
-    static WeaverClassPool createClassPool(URLClassLoader classLoader, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs) {
-        WeaverClassPool pool = new WeaverClassPool(classLoader)
-        pool.childFirstLookup = true
-        pool.appendSystemPath()
-        inputs.each {
-            it.jarInputs.each {
-                pool.appendClassPath(it.file)
-            }
-            it.directoryInputs.each {
-                pool.appendClassPath(it.file)
-            }
-        }
-        referencedInputs.each {
-            it.jarInputs.each {
-                pool.appendClassPath(it.file)
-            }
-            it.directoryInputs.each {
-                pool.appendClassPath(it.file)
-            }
-        }
-        return pool
-    }
-
-    /**
-     * @param inputs the inputs provided by transform API.
-     * @return Returns all .class files from project source set.
-     */
-    private static Set<File> getClassFiles(Collection<TransformInput> inputs) {
-        Set<File> classFiles = new HashSet<File>()
-        inputs.each {
-            it.directoryInputs.each {
-                it.file.eachFileRecurse(FileType.FILES) {
-                    if (it.absolutePath.endsWith(SdkConstants.DOT_CLASS)) {
-                        classFiles.add(it)
-                    }
-                }
-            }
-            it.jarInputs.each {
-                def jarFile = new JarFile(it.file)
-                jarFile.entries().findAll {
-                    !it.directory && it.name.endsWith(SdkConstants.DOT_CLASS)
-                }.each {
-                    classFiles.add(new File(it.name))
-                }
-            }
-        }
-        return classFiles
-    }
-
-    /**
-     * @param outputProvider
-     * @return Returns a directory which will store transformed classes.
-     */
-    private File getOutputFile(TransformOutputProvider outputProvider) {
-        return outputProvider.getContentLocation(
-                'weaver', getInputTypes(), getScopes(), Format.DIRECTORY)
-    }
-
-    private File getSecondaryOutputFile() {
-        return project.file("$project.buildDir/weaver/")
-    }
-
-    /**
-     * Copies all non-class files.
-     * @param inputs
-     * @param outputProvider
-     * @return
-     */
-    private copyResourceFiles(Collection<TransformInput> inputs, TransformOutputProvider outputProvider) {
-        inputs.each {
-            it.directoryInputs.each {
-                def dirPath = it.file.absolutePath
-                it.file.eachFileRecurse(FileType.FILES) {
-                    if (!it.absolutePath.endsWith(SdkConstants.DOT_CLASS)) {
-                        log "Copying resource ${it}"
-                        def dest = new File(getOutputFile(outputProvider),
-                                it.absolutePath.substring(dirPath.length()))
-                        dest.parentFile.mkdirs()
-                        Files.copy(it, dest)
-                    }
-                }
-            }
-            // no need to implement the code for `it.jarInputs.each` since PROJECT SCOPE does not use jar input.
-        }
-    }
-
-    void log(String message) {
-        logger.info "[Weaver] $message"
-    }
+  void log(String message) {
+    logger.info "[Weaver] $message"
+  }
 }
